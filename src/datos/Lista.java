f8373f0e09512db964521aab9ab17451b1fbc271
AlfaLobo <57053999+AlfaLobo@users.noreply.github.com>
2019-10-31 23:48:06 -0600

    Delete Lista.java

--- src/datos/Lista.java
+++
@@ -1,9 +0,0 @@
-package datos;
-import java.util.ArrayList;
-import java.util.List;
-
-public class Lista {
-    List<Cliente> clientes = new ArrayList<>();
-    List<Producto> productos = new ArrayList<>();
-    List<Proveedor> proveedores = new ArrayList<>();
-}
