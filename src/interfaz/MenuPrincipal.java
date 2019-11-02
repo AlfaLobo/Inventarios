29806b1b5a0e3788e9a20f62780b5ba3b983a5a1
AlfaLobo <57053999+AlfaLobo@users.noreply.github.com>
2019-11-01 19:51:57 -0600

    Add files via upload

--- src/interfaz/MenuPrincipal.java
+++ src/interfaz/MenuPrincipal.java
@@ -1,15 +1,32 @@
 package interfaz;
 
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class MenuPrincipal {
     JFrame f = new JFrame("temp");
+    JButton b = new JButton("UvU");
     JLabel l = new JLabel("Bienvenido OvO");
     public MenuPrincipal() {
         f.setSize(400,500);
         l.setBounds(30,145,400, 40);
+        b.setBounds(30,175,100, 40);
         f.setLayout(null);
         f.setVisible(true);
         f.add(l);
+        f.add(b);
+        b.addActionListener(new ActionListener(){
+            String UvU = "UvU";
+            int i = 70;
+            int j = 145;
+            public void actionPerformed(ActionEvent e){
+                l.setText(UvU);
+                l.setBounds(i,j,400, 40);
+                UvU=UvU+"vU";
+                i++;
+                j--;
+            }
+        });
     }
 }
