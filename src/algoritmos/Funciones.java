2fc09a7df2e7ed054eecf4eb95fff61cb07aa9b7
AlfaLobo <57053999+AlfaLobo@users.noreply.github.com>
2019-11-05 23:28:39 -0600

    Update Funciones.java

--- src/algoritmos/Funciones.java
+++ src/algoritmos/Funciones.java
@@ -1,7 +1,30 @@
 package algoritmos;
 
+import javax.print.*; 
+//doc
+//DocFlavor
+//PrintException
+//printService
+//              Lookup
+//SimpleDoc
+
+
 public class Funciones {
     public static void imprimirRecibo() {
+        /*
+         *PARA IMPRIMIR EN IMPRESORA
+         */
+        //String texto = "Texto";
+        PrintService printService = PrintServiceLookup.lookupDefaultPrintService();
+        DocPrintJob docPintJob = printService.createPrintJob();
+        Doc doc = new SimpleDoc(text.getBytes(), flavor, null);
+        try{
+            docPrintJob.print(doc, null); 
+        }
+        catch (Esxception e){
+           e.printStackTrace();
+        }
+        
 
     }
 }
